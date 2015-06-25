215a2e7cff97071a85ccefd351d9581fcb911f95
Steven McGrath <smcgrath@tenable.com>
2015-06-25 09:44:37 -0500

    more settings file tweaking...

--- app/defaults.py
+++ app/defaults.py
@@ -7,5 +7,6 @@ SECRET_KEY = 'This is the default key INSECURE!'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'database.db')
 SQLALCHEMY_MIGRATE_REPO = 'db_repository'
 
-if os.path.exists('settings.py'):
+settings_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'settings.py')
+if os.path.exists(settings_file):
     from settings import *
