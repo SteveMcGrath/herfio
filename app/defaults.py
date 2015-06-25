bd463e7c6d31860f7efb151b19e8e0d8879c9d7e
Steven McGrath <smcgrath@tenable.com>
2015-06-25 01:29:09 -0500

    Removed SERVER_NAME as all it did was cause issues.

--- app/defaults.py
+++ app/defaults.py
@@ -6,4 +6,3 @@ TESTING = True
 SECRET_KEY = 'This is the default key INSECURE!'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'database.db')
 SQLALCHEMY_MIGRATE_REPO = 'db_repository'
-SERVER_NAME = 'localhost:5000'
