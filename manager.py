686a3bb5c4b761cebecb86da902af53939c7103f
Steven McGrath <smcgrath@tenable.com>
2015-06-25 01:24:43 -0500

    Added route listing command to manager.

--- manager.py
+++ manager.py
@@ -9,6 +9,25 @@ app = create_app()
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def list_routes():
+    import urllib
+    from flask import url_for
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+    
+    for line in sorted(output):
+        print line
+
 # These are the parsers that we will be using...
 from app.parsers import parsers
 
