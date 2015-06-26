958f7eabf62c7ec1d808c028b494117f940334dd
Steven McGrath <smcgrath@tenable.com>
2015-06-26 06:05:03 -0500

    Broke out the page into multiple templates and switched to using the standard deviation.

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from .extensions import db
 from .models import Auction
 from .forms import SearchForm
 from urllib2 import unquote
+from time import mktime
 from math import floor
 from numpy import median, std
 
@@ -12,45 +13,40 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/search/<search_string>')
+@app.route('/search/')
 def search(search_string=None):
     '''Search Page'''
     form = SearchForm()
-    results = None
-    stats = {}
+    auctions = None
+    stats = {'display': False}
     
     if form.validate_on_submit():
         return redirect('/search/%s' % form.search.data)
 
     if search_string:
+        print search_string
         search_string = unquote(search_string).decode('utf8')
-        if search_string == '':
-            search_string = '[EMPTY]'
-        else:
-            o = Auction.query.order_by(Auction.name).filter_by(finished=False)
-            c = Auction.query.order_by(Auction.close).filter_by(finished=True)
-            for word in search_string.split():
-                o = o.filter(Auction.name.like('%%%s%%' % word))
-                c = c.filter(Auction.name.like('%%%s%%' % word))
-            closed_auctions = c.all()
-            open_auctions = o.all()
+        #if search_string == '':
+        #    search_string = '[EMPTY]'
+        #else:
+        a = Auction.query.order_by(Auction.name)
+        for word in search_string.split():
+            a = a.filter(Auction.name.like('%%%s%%' % word))
+        auctions = a.all()
+        print len(auctions)
 
-        if closed_auctions:
-            prices = sorted([i.price_per_stick for i in closed_auctions if i.price_per_stick is not None])
+        if auctions:
+            prices = sorted([i.price_per_stick for i in auctions if i.price_per_stick is not None and i.finished])
             if len(prices) > 0:
+                stats['display'] = True
                 stats['avg_price'] = sum(prices)/len(prices)
-                stats['median_price'] = median(prices)
                 stats['std_deviation'] = std(prices)
-                stats['worst_price'] = min(prices)
-                stats['bad_price'] = prices[floor(len(prices)*5/6)]
-                stats['poor_price'] = prices[floor(len(prices)*4/6)]
-                stats['good_price'] = prices[floor(len(prices)*2/6)]
-                stats['great_price'] = prices[floor(len(prices)*1/6)]
-                stats['best_price'] = max(prices)
-            stats['trend'] = [[mktime(i.close.timetuple()), i.price_per_stick] for i in closed_auctions if i.price_per_stick is not None]            
+                stats['worst_price'] = prices[-1]
+                stats['best_price'] = prices[0]
+            stats['trend'] = [[mktime(i.close.timetuple()), float(i.price_per_stick)] for i in auctions if i.price_per_stick is not None and i.finished]            
 
     return render_template('search.html',
-        open_auctions=open_auctions,
-        closed_auctions=cloased_auctions, 
+        auctions=auctions,
         form=form,
         stats=stats,
         search_string=search_string,
