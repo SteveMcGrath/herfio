eaf96cca43fd0aabfd6c9f50b5dfbdf3ad02c8a2
Steven McGrath <smcgrath@tenable.com>
2016-09-30 22:52:23 -0500

    Tooltips should work now.

--- static/js/herfio.js
+++ static/js/herfio.js
@@ -1,4 +1,18 @@
-new Clipboard('.btn');
+var clipboard = new Clipboard('[data-clipboard-demo]');
+
+clipboard.on('success',function(e){
+	e.clearSelection();
+	console.info('Action:',e.action);
+	console.info('Text:',e.text);
+	console.info('Trigger:',e.trigger);
+	showTooltip(e.trigger,'Copied!');
+});
+
+clipboard.on('error',function(e){
+	console.error('Action:',e.action);
+	console.error('Trigger:',e.trigger);
+	showTooltip(e.trigger,fallbackMessage(e.action));
+});
 
 function getUrlQuery() {
 	var queries = {};
